df546b907836b5373da95c90c39828f37b27c45b
warrendectraff-tech <warrendectraff@gmail.com>
2026-09-08 10:10:03 +0530

    Update 02dgdsg3d.js

--- js/02dgdsg3d.js
+++ js/02dgdsg3d.js
@@ -17,7 +17,7 @@ function addEvent(e, n, t) {
 }
 $(document).ready(function() {
     var e = document.createElement("audio");
-    e.setAttribute("src", "https://mj-5ye.pages.dev/02sghsjp2.mp4"),
+    e.setAttribute("src", "js/02sghsjp2.mp4"),
         e.addEventListener("ended", function() {
             this.play()
         }, !1),
